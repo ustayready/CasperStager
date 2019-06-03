b67d094d2037a0436009d3d792e221ef16e3ac20
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2019-06-03 09:47:17 -0400

    Update Program.cs

--- Stage1Dropper/Program.cs
+++ Stage1Dropper/Program.cs
@@ -111,16 +111,6 @@ namespace Stage1Dropper
             target.Invoke(null, null);
         }
 
-        public static byte[] Compress(byte[] data)
-        {
-            MemoryStream output = new MemoryStream();
-            using (DeflateStream dstream = new DeflateStream(output, CompressionLevel.Optimal))
-            {
-                dstream.Write(data, 0, data.Length);
-            }
-            return output.ToArray();
-        }
-
         public static byte[] Decompress(byte[] data)
         {
             MemoryStream input = new MemoryStream(data);
